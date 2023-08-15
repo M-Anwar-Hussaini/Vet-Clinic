29c93684260700ed080bf924b69712496e81da80
Anwar <anwarhussaini160@gmail.com>
2023-08-15 05:35:49 +0430

    Add animals table

--- schema.sql
+++ schema.sql
@@ -1,2 +1,10 @@
-/* Database schema to keep the structure of entire database. */
 CREATE DATABASE vet_clinic;
+
+CREATE TABLE animals (
+  id INTEGER NOT NULL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  date_of_birth DATE,
+  escape_attemps INTEGER,
+  neutered BOOLEAN,
+  weight_kg DECIMAL(10, 2)
+);
