39d3bdb6b3e075e3f1ba4604cadcb9e4035040af
Anwar <anwarhussaini160@gmail.com>
2023-08-15 05:57:27 +0430

    Add data to the animals table

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ CREATE TABLE animals (
   id BIGSERIAL  PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   date_of_birth DATE,
-  escape_attemps INTEGER,
+  escape_attempts INTEGER,
   neutered BOOLEAN,
   weight_kg DECIMAL(10, 2)
 );
