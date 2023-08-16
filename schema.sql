fdd30e48f97d74829e117528d34f0f638eda8585
Anwar <anwarhussaini160@gmail.com>
2023-08-16 17:38:46 +0430

    Add new owners and species tables and update the animal tables to have foreign keys

--- schema.sql
+++ schema.sql
@@ -20,3 +20,42 @@ CREATE TABLE animals (
 
 ALTER TABLE animals
 ADD COLUMN species VARCHAR(50);
+
+
+-- ---------------- --
+-- THIRD MILESTONE --
+-- ---------------- --
+
+CREATE TABLE owners (
+	id SERIAL PRIMARY KEY,
+	full_name VARCHAR(50),
+	age SMALLINT
+);
+
+CREATE TABLE species (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50)
+);
+
+-- Delete the species column
+ALTER TABLE animals
+DROP COLUMN species;
+
+-- Add species_id column
+ALTER TABLE animals
+ADD COLUMN species_id INTEGER;
+
+-- Add foreign key to species_id column
+ALTER TABLE animals
+ADD CONSTRAINT fk_animals_species
+FOREIGN KEY (species_id) REFERENCES species(id);
+
+
+-- Add owner_id column to animals table
+ALTER TABLE animals
+ADD COLUMN owner_id INTEGER;
+
+-- Add foreign key to owner_id column
+ALTER TABLE animals
+ADD CONSTRAINT fk_animals_owners
+FOREIGN KEY (owner_id) REFERENCES owners(id);
