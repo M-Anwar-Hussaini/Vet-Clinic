3425baa24a14fbfe705ceb2a405780f79271e803
Anwar <anwarhussaini160@gmail.com>
2023-08-16 17:48:38 +0430

    Add some data to owners table

--- data.sql
+++ data.sql
@@ -46,3 +46,19 @@ VALUES ('Blossom', '1998-10-13', 3, TRUE, 17);
 -- Insert the animal "Ditto"
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES ('Ditto', '2022-05-14', 4, TRUE, 22);
+
+
+
+-- ---------------- --
+-- THIRD MILESTONE --
+-- ---------------- --
+
+-- Add data to owners table
+INSERT INTO owners (full_name, age)
+VALUES
+	('Sam Smith', 34),
+	('Jennifer Orwell', 19),
+	('Bob', 45),
+	('Melody Pond', 77),
+	('Dean Winchester', 14),
+	('Jodie Whittaker', 38);
