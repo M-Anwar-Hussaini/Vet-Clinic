45295cf626c3504a6829d3903efa8841d88f713f
Anwar <anwarhussaini160@gmail.com>
2023-08-22 02:47:19 +0430

    Add more than 3.5 million records on visits table

--- data.sql
+++ data.sql
@@ -132,3 +132,15 @@ VALUES
 	('2020-5-24', 3, 10),
 	('2021-1-11', 1, 10);
 	
+
+
+-- ---------------- --
+-- FIFTH MILESTONE --
+-- ---------------- --
+
+-- This will add 3.594.280 visits considering you have 10 animals,
+-- 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animal_id, vet_id, date_of_visit)
+	SELECT * FROM (SELECT id FROM animals) animal_ids,
+	(SELECT id FROM vets) vets_ids,
+	generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
