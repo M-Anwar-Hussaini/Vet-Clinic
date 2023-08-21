e78ac482d2a5ce63ee3f965813ccc549fe5987a5
demesameneshoa <demesameneshoa@gmail.com>
2023-08-22 02:50:40 +0300

    add indexes to tables to improve performance

--- schema.sql
+++ schema.sql
@@ -95,3 +95,12 @@ CREATE TABLE visits (
 
 -- Add an email column to your owners table
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+-- add index for a visits table by animal id
+CREATE INDEX idx_animal_id ON visits (animal_id);
+
+-- add index for a visits table by vet id
+CREATE INDEX idx_vet_id ON visits (vet_id);
+
+--add index for owners table by email
+CREATE INDEX idx_email ON owners (email);
